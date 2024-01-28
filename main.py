03bbb30993092497e95846257beb90a5021096df
Samuel <samuelrivba@gmail.com>
2024-01-28 14:03:34 -0500

    Add final project

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Welcome to the tip claculator.")
+bill = float(input("What was the total bill? "))
+tip = int(input("What percentage tip would you like to give? 10, 12, or 15? ")) / 100 
+people = int(input("How many people to split the bill? "))
+bill_tip = bill + (bill*tip)
+
+formatted = "{:.2f}".format(round(bill_tip/people, 2))
+
+print(f"Each person should pay: ${formatted}")
